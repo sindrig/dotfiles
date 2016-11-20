8fce39c7d5acf2287e69b9d7604e7567927c7535
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2016-11-20 15:43:41 +0000

    kk

--- bin/create_sublime_project.sh
+++ bin/create_sublime_project.sh
@@ -9,7 +9,6 @@ PROJECT_NAME=$1
 PROJECT_DIRECTORY="$( cd $2 && pwd )"
 
 SUBLIME_PROJET_DIRECTORY="~/.config/sublime-text-3/Packages/User/Projects/"
-SUBLIME_PROJET_DIRECTORY="~/tmp/"
 TARGET_FILENAME="$SUBLIME_PROJET_DIRECTORY$PROJECT_NAME.sublime-project"
 eval TARGET_FILENAME=$TARGET_FILENAME
 
@@ -17,4 +16,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SAMPLE_FILENAME="sample-project.sublime-project"
 
 PROJECT_CONTENTS=$(cat $DIR/$SAMPLE_FILENAME | sed "s_PATH\_TO\_PROJECT_$2_")
+echo $TARGET_FILENAME
 echo "$PROJECT_CONTENTS" > "$TARGET_FILENAME"
