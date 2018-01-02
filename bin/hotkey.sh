6eba50551db8712940617b7ba1c0833a63bb1a21
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2018-01-02 12:47:45 +0000

    Automagically change xrandr output when docking/undocking

---
+++ bin/hotkey.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+sleep 0.5
+WS=/home/sindri/bin/ws
+MONITOR_COUNT=$(xrandr --current -d :0| grep connected | grep -v disconnected | wc -l)
+
+CRAZY=$4
+echo "CRAZY $CRAZY" >> /tmp/docklogger
+
+if [ "$CRAZY" != "00006030" ]; then
+    exit 0
+fi
+
+echo "$@" >> /tmp/docklogger
+echo "MONITOR_COUNT $MONITOR_COUNT" >> /tmp/docklogger
+
+
+
+case "$MONITOR_COUNT" in
+    "1")
+        XRANDR_FLAGS="-d :0" $WS single >> /tmp/docklogger 2>&1
+        ;;
+    "2")
+        XRANDR_FLAGS="-d :0" $WS home >> /tmp/docklogger 2>&1
+        ;;
+    "3")
+        XRANDR_FLAGS="-d :0" $WS wrk >> /tmp/docklogger 2>&1
+        ;;
+esac
+exit 0
