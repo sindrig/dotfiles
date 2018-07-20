d640d42d277bab3f910b2aad483791fbef447699
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2018-07-20 12:41:45 +0000

    yaya

--- bin/hotkey.sh
+++ bin/hotkey.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sleep 0.5
 WS=/home/sindri/bin/ws
-MONITOR_COUNT=$(xrandr --current -d :0| grep connected | grep -v disconnected | wc -l)
+MONITOR_COUNT=$(XAUTHORITY=/home/sindri/.Xauthority xrandr --current -d :0| grep connected | grep -v disconnected | wc -l)
 
 CRAZY=$4
 echo "CRAZY $CRAZY" >> /tmp/docklogger
@@ -15,13 +15,13 @@ echo "MONITOR_COUNT $MONITOR_COUNT" >> /tmp/docklogger
 
 case "$MONITOR_COUNT" in
     "1")
-        XRANDR_FLAGS="-d :0" $WS single >> /tmp/docklogger 2>&1
+        XAUTHORITY=/home/sindri/.Xauthority XRANDR_FLAGS="-d :0" $WS single >> /tmp/docklogger 2>&1
         ;;
     "2")
-        XRANDR_FLAGS="-d :0" $WS home >> /tmp/docklogger 2>&1
+        XAUTHORITY=/home/sindri/.Xauthority XRANDR_FLAGS="-d :0" $WS home >> /tmp/docklogger 2>&1
         ;;
     "3")
-        XRANDR_FLAGS="-d :0" $WS wrk >> /tmp/docklogger 2>&1
+        XAUTHORITY=/home/sindri/.Xauthority XRANDR_FLAGS="-d :0" $WS wrk >> /tmp/docklogger 2>&1
         ;;
 esac
 exit 0
