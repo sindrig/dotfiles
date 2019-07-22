0797ea67d0fad85b53f14c9a88a7cb42a7501701
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2019-07-22 14:51:34 +0000

    minor adjustments

--- arch-install.sh
+++ arch-install.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
+set -e
 sudo pacman -S git
-
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
+(
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+	makepkg -si
+	cd ..
+	rm -r yay
+)
 
 curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
 echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/dev/x86_64" | sudo tee -a /etc/pacman.conf
-
-pacman -S - < pkglist.txt
+yay -Sy
+yay -S - < pkglist.txt
 
 sudo gpasswd -a sindri bumblebee
-sudo systemctl enable bumblebee
+sudo systemctl enable bumblebeed
