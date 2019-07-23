e1231918040fbc45f27d82d0d8ba4052e86c9761
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2019-07-23 09:23:09 +0000

    Update download_ruv_program.py

--- bin/download_ruv_program.py
+++ bin/download_ruv_program.py
@@ -537,7 +537,6 @@ def main(args):
 
 
 if __name__ == '__main__':
-    logger.warning('Running download ruv program with args: %s', sys.argv)
     parser = argparse.ArgumentParser()
     query_arg = parser.add_argument(
         'query',
