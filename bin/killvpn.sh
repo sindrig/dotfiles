86241be53b7b8c06669a6a86ee9bb80bba3af644
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2020-09-08 23:35:03 +0000

    First savepoint for regolith

--- bin/killvpn.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-killall $1 openvpn
--- config/i3blocks/ovpn.sh
+++
@@ -1,9 +0,0 @@
-if [ -d "/proc/sys/net/ipv4/conf/vpn0" -o -f "/var/run/vpnc.pid" ]; then
-    echo 'VPN: up'
-    echo 'VPN: up'
-    echo \#00FF00
-else
-    echo 'VPN: down'
-    echo 'VPN: down'
-    echo \#FF0000
-fi
