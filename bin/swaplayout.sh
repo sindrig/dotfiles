d406bb416f596246ab67b4005555aaad50eb623b
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2021-01-19 00:00:07 +0000

    manjarooo

--- bin/swaplayout.sh
+++ bin/swaplayout.sh
@@ -2,9 +2,9 @@
 LG=$(setxkbmap -query | awk '/layout/{print $2}')
 if [ "$LG" == "is" ]
 then
-    setxkbmap us
+    setxkbmap -model pc105 -layout us
     echo "Layout: us"
 else
-    setxkbmap is
+    setxkbmap -model pc105 -layout is
     echo "Layout: is"
 fi
