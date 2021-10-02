83a56bcafed7b28e0ad64fb3f5b55799b4289ba6
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2021-10-02 13:13:38 +0000

    Cleanup for sway

--- bin/on-song-change.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-status=$(baton status)
-song_name=`echo "$status" | grep '^Track' | cut -d" " -f2-`
-artist=`echo "$status" | grep '^Artist' | cut -d" " -f2-`
-echo "$song_name by $artist" > ~/.current-spotify-song
-echo "$song_name by $artist" > ~/.current-spotify-song
