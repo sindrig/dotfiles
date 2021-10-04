0797c53f502a270de4f52aefd7ed0f57d993f82c
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2021-10-04 11:16:09 +0000

    monitors and stuff

--- bin/swaplayout.sh
+++ bin/swaplayout.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-KEYBOARD="1:1:AT_Translated_Set_2_keyboard"
-LG=$(swaymsg -t get_inputs | jq -r '.[] | select(.identifier == "'"$KEYBOARD"'") | .xkb_active_layout_name')
+LG=$(swaymsg -t get_inputs | jq -r '.[].xkb_active_layout_name' | sed -e '/^null$/d' | uniq)
 if [ "$LG" == "Icelandic" ]
 then
-    swaymsg input "$KEYBOARD" xkb_layout us -q
+    swaymsg input '*' xkb_layout us -q
     echo "Layout: us"
 else
-    swaymsg input "$KEYBOARD" xkb_layout is -q
+    swaymsg input '*' xkb_layout is -q
     echo "Layout: is"
 fi
