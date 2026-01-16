62026df44994330ce79fada82d8ac7095be94828
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2026-01-16 16:00:18 +0000

    disable inlay hints

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -3,8 +3,20 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        inlay_hints = { enabled = false },
         pyright = false,
         basedpyright = false,
+        ty = {
+          settings = {
+            ty = {
+              inlayHints = {
+                variableTypes = false,
+                callArgumentNames = false,
+                functionReturnTypes = false,
+              },
+            },
+          },
+        },
         -- ty = {
         --   -- If lspconfig has a built-in 'ty' server entry, this will work.
         --   -- Otherwise, use the fallback autostart below.
