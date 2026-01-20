cd73500c75e2c705808a5d9bf452b33d18f5a9b3
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2026-01-20 18:53:20 +0000

    back to pyright for now

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -2,21 +2,21 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      inlay_hints = { enabled = false },
       servers = {
-        inlay_hints = { enabled = false },
-        pyright = false,
-        basedpyright = false,
-        ty = {
-          settings = {
-            ty = {
-              inlayHints = {
-                variableTypes = false,
-                callArgumentNames = false,
-                functionReturnTypes = false,
-              },
-            },
-          },
-        },
+        -- pyright = false,
+        -- basedpyright = false,
+        -- ty = {
+        --   settings = {
+        --     ty = {
+        --       inlayHints = {
+        --         variableTypes = false,
+        --         callArgumentNames = false,
+        --         functionReturnTypes = false,
+        --       },
+        --     },
+        --   },
+        -- },
         -- ty = {
         --   -- If lspconfig has a built-in 'ty' server entry, this will work.
         --   -- Otherwise, use the fallback autostart below.
