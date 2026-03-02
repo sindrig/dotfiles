cad6588d242500805cf5681c0cb72ab5047cb17b
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2026-03-02 14:58:12 +0000

    enable ty again

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -4,9 +4,9 @@ return {
     opts = {
       inlay_hints = { enabled = false },
       servers = {
-        -- pyright = false,
-        -- basedpyright = false,
-        ty = false,
+        pyright = false,
+        basedpyright = false,
+        -- ty = false,
         -- ty = {
         --   settings = {
         --     ty = {
